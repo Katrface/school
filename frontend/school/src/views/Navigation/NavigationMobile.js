15ad6c995052e14a7d2f12571ea8f8bfc6eb16af
Katrface <maksim_dzdanov@mail.ru>
2021-04-06 02:02:18 +0300

    add closing menu on click navigation

--- frontend/school/src/views/Navigation/NavigationMobile.js
+++ frontend/school/src/views/Navigation/NavigationMobile.js
@@ -8,11 +8,18 @@ const NavigationMobile = (props) => {
     const links = props.links;
     const menuViewMode = props.menuViewMode;
 
+    const closeMenu = () => {
+        if (menuViewMode.isOpen) {
+            menuViewMode.changeMode();
+        }
+    }
+
     const bottomMenuCells = links.map((linkObj) => {
         const iconClassName = linkObj.iconClass + " bottom-menu__icon";
         return (
             <NavigationMobileCell key={linkObj.name}>
                 <NavLink
+                    onClick={closeMenu}
                     exact to={linkObj.link}
                     isActive={(match, location) => {
                         return !menuViewMode.isOpen && (location.pathname === linkObj.link);
